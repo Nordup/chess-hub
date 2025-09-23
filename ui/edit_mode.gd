773beeef4f5bbd582149a1ee836804939c2a786e
Nordup <nordup.ondr@gmail.com>
2025-09-23 16:18:29 +0700

    bring content from welcome page

--- ui/edit_mode.gd
+++ ui/edit_mode.gd
@@ -2,7 +2,17 @@ extends Node
 class_name EditMode
 
 static var is_enabled: bool
+static var is_connection_msg: bool
+static var is_show_mouse: bool
 
 
 static func set_enabled(value: bool) -> void:
 	is_enabled = value
+
+
+static func set_connection_msg(value: bool) -> void:
+	is_connection_msg = value
+
+
+static func set_show_mouse(value: bool) -> void:
+	is_show_mouse = value
--- ui/mouse_mode.gd
+++ ui/mouse_mode.gd
@@ -1,6 +1,5 @@
 extends Control
-
-var is_in_game: bool
+class_name MouseMode
 
 
 func _ready() -> void:
@@ -9,8 +8,6 @@ func _ready() -> void:
 
 
 func _input(_event: InputEvent) -> void:
-	if not is_in_game: return
-	
 	if Input.is_action_just_pressed("show_mouse"): set_captured(false)
 	if Input.is_action_just_released("show_mouse"): set_captured(true)
 
@@ -18,16 +15,15 @@ func _input(_event: InputEvent) -> void:
 func on_visibility_changed() -> void:
 	if is_visible_in_tree():
 		set_captured(true)
-		is_in_game = true
 	else:
 		set_captured(false)
-		is_in_game = false
 
 
 func _notification(what: int) -> void:
 	match what:
 		MainLoop.NOTIFICATION_APPLICATION_FOCUS_IN:
-			if is_visible_in_tree(): set_captured(true)
+			if is_visible_in_tree() and not EditMode.is_show_mouse:
+				set_captured(true)
 		MainLoop.NOTIFICATION_APPLICATION_FOCUS_OUT:
 			pass
 
